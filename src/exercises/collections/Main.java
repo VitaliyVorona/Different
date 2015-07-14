8cefcdeae0fa243239371a34537f523218b26ffb
Vitaliy Vorona <vitaliy.vorona@avid.com>
2015-07-14 20:23:17 +0300

    Init commit "hello world"

---
+++ src/exercises/collections/Main.java
@@ -0,0 +1,10 @@
+package exercises.collections;
+
+/**
+ * Created by vitaliy.vorona on 7/14/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
